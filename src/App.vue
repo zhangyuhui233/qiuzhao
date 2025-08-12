d3601e34bc83d1d8b81d721ed5d5ca66c262a5c0
zhangyuhui <zhangyuhui@uino.com>
2025-08-12 09:05:19 +0800

    qiuzhaozhunbei

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+
+
+</script>
+<style scoped></style>
